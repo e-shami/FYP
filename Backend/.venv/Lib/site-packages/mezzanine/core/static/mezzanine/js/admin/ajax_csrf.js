84f86a0db8cf1bcc191b1770568ddc9baa683018
Ehtisham Rehmat <a7rehmat@gmail.com>
2023-12-04 04:25:17 +0500

    Update .gitignore to exclude unnecessary directories

--- Backend/.venv/Lib/site-packages/mezzanine/core/static/mezzanine/js/admin/ajax_csrf.js
+++
@@ -1,8 +0,0 @@
-
-jQuery(function($) {
-    $.ajaxSetup({
-        beforeSend: function(xhr, settings) {
-            xhr.setRequestHeader('X-CSRFToken', window.__csrf_token);
-        }
-    });
-});
