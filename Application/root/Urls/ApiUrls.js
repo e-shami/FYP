f1048de082c6b4e978b68c59e721c21acf4bbf2b
Ehtisham Rehmat <a7rehmat@gmail.com>
2023-12-08 10:40:13 +0500

    ip changed for attah's iphone for presentation

--- Application/root/Urls/ApiUrls.js
+++ Application/root/Urls/ApiUrls.js
@@ -3,7 +3,7 @@ let MainUrl = "http://"+Domain+"/api/"
 let  WsUrl = `ws://${Domain}/`
 let isDev = true;
 if(isDev){
-    Domain = "192.168.0.119:8000"
+    Domain = "172.20.10.5:8000"
     MainUrl = "http://"+Domain+"/api/"
     WsUrl = `ws://${Domain}/`
 }
--- Arduino/waterTankApiEsp8266/waterTankApiEsp8266.ino
+++ Arduino/waterTankApiEsp8266/waterTankApiEsp8266.ino
@@ -2,10 +2,10 @@
 #include <ESP8266HTTPClient.h>
 
 
-const String DATA_POST_URL = "http://192.168.0.119:8000/api/update-tank-level/";
+const String DATA_POST_URL = "http://172.20.10.5:8000/api/update-tank-level/";
 
-const char *ssid = "Apni Auqat";
-const char *password = "gulugulu123";
+const char *ssid = "Attah's iPhone";
+const char *password = "kaka1234";
 
 const int API_REQUEST_DELAY_IN_MICROSECONDS = 1000 * 5 * 1 ;
 
