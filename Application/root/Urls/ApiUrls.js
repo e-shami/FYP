a3fee2e8981e58bbc10c8c16377ade9d202ab3d0
Ehtisham Rehmat <a7rehmat@gmail.com>
2024-01-06 02:53:34 +0500

    home ip added to test run eas build

--- Application/root/Urls/ApiUrls.js
+++ Application/root/Urls/ApiUrls.js
@@ -3,6 +3,7 @@ let MainUrl = "http://"+Domain+"/api/"
 let  WsUrl = `ws://${Domain}/`
 let isDev = true;
 if(isDev){
+    // adding home ip
     Domain = "192.168.0.114:8000"
     MainUrl = "http://"+Domain+"/api/"
     WsUrl = `ws://${Domain}/`
